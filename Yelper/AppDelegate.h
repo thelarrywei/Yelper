2d384cfe5e5849815ac59a2b201e4e7193d7e2b7
thelarrywei <thelarrywei@gmail.com>
2014-10-22 10:01:35 -0700

    Initial Commit

---
+++ Yelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelper
+//
+//  Created by Larry Wei on 10/22/14.
+//  Copyright (c) 2014 Larry Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YelperTests/YelperTests.m
@@ -0,0 +1,40 @@
+//
+//  YelperTests.m
+//  YelperTests
+//
+//  Created by Larry Wei on 10/22/14.
+//  Copyright (c) 2014 Larry Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface YelperTests : XCTestCase
+
+@end
+
+@implementation YelperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
